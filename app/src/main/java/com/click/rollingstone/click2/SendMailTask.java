c1cc2832be91ecad2a5099da936932701218d987
dharabadani <dharapbadani@gmail.com>
2015-10-10 18:20:09 +0530

    Password Encryption

--- app/src/main/java/com/click/rollingstone/click2/SendMailTask.java
+++ app/src/main/java/com/click/rollingstone/click2/SendMailTask.java
@@ -8,14 +8,19 @@ import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
-import android.util.Log;
+import android.util.Base64;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.security.MessageDigest;
+import java.security.spec.AlgorithmParameterSpec;
 import java.util.Properties;
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import javax.mail.AuthenticationFailedException;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -45,7 +50,14 @@ import javax.mail.util.ByteArrayDataSource;
 
             Session session = Session.getInstance(props, new javax.mail.Authenticator() {
                 protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("smarteyeintrusion@gmail.com", "dasayan5");
+                    String pwd = "";
+                    try {
+                        SendMailTask.AESCrypt("encrypt");
+                        pwd = SendMailTask.decrypt("6mlBnct9kAWeXP9LHnupuA==\n");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return new PasswordAuthentication("smarteyeintrusion@gmail.com", pwd);
                 }
             });
             this.msg = new MimeMessage(session);
@@ -167,6 +179,57 @@ import javax.mail.util.ByteArrayDataSource;
          if (networkInfo.isAvailable() == false)
              return false;
          return true;
+     }
+
+     private static Cipher cipher;
+     private static SecretKeySpec key;
+     private static AlgorithmParameterSpec spec;
+
+
+     public static void AESCrypt(String password) throws Exception
+     {
+         // hash password with SHA-256 and crop the output to 128-bit for key
+         MessageDigest digest = MessageDigest.getInstance("SHA-256");
+         digest.update(password.getBytes("UTF-8"));
+         byte[] keyBytes = new byte[32];
+         System.arraycopy(digest.digest(), 0, keyBytes, 0, keyBytes.length);
+
+         cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+         key = new SecretKeySpec(keyBytes, "AES");
+         spec = getIV();
+     }
+
+     public static AlgorithmParameterSpec getIV()
+     {
+         byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
+         IvParameterSpec ivParameterSpec;
+         ivParameterSpec = new IvParameterSpec(iv);
+
+         return ivParameterSpec;
+     }
+
+     /*public static String encrypt(String plainText) throws Exception
+     {
+         cipher.init(Cipher.ENCRYPT_MODE, key, spec);
+         byte[] encrypted = cipher.doFinal(plainText.getBytes("UTF-8"));
+         String encryptedText = new String(Base64.encode(encrypted, Base64.DEFAULT), "UTF-8");
+
+         return encryptedText;
+     }*/
+
+     public static String decrypt(String cryptedText) throws Exception
+     {
+         cipher.init(Cipher.DECRYPT_MODE, key, spec);
+         byte[] bytes = Base64.decode(cryptedText, Base64.DEFAULT);
+         byte[] decrypted = cipher.doFinal(bytes);
+         String decryptedText = new String(decrypted, "UTF-8");
+
+         return decryptedText;
+     }
+
+     public static void main(){
+         SendMailTask sm = new SendMailTask("");
+
      }
  }
 
